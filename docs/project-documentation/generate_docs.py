17f3f3ee0faac00fb6be757188967cd1f7fcf748
lproject012125 <lproject012125@gmail.com>
2026-09-07 22:45:56 +0800

    Revert "Point documentation GitHub links to rproject1324/diari-core and regenerate PDF"
    This reverts commit 56ca27d2700bdc500add77b59e1a6bd2d426cc17.

--- docs/project-documentation/generate_docs.py
+++ docs/project-documentation/generate_docs.py
@@ -200,7 +200,8 @@ def build():
     story.append(Paragraph("<b>Tolentino, Lawrence Dave P.</b>", s["CoverMeta"]))
     story.append(Spacer(1, 16 * mm))
     story.append(Paragraph("Live application: https://diaricore.up.railway.app/", s["CoverMeta"]))
-    story.append(Paragraph("GitHub: https://github.com/rproject1324/diari-core", s["CoverMeta"]))
+    story.append(Paragraph("GitHub (workspace origin): https://github.com/lproject012125/diari-core", s["CoverMeta"]))
+    story.append(Paragraph("GitHub (README remote): https://github.com/0323-3621-cell/diaricore", s["CoverMeta"]))
     story.append(PageBreak())
 
     # TOC
@@ -815,7 +816,8 @@ def build():
         ["Resource", "URL"],
         [
             ["Live app (README)", "https://diaricore.up.railway.app/"],
-            ["GitHub repository", "https://github.com/rproject1324/diari-core"],
+            ["GitHub (this workspace origin)", "https://github.com/lproject012125/diari-core"],
+            ["GitHub (README remote)", "https://github.com/0323-3621-cell/diaricore"],
             ["Model Hub (current code default)", "https://huggingface.co/sseia/diari-core-mood"],
             ["Inference Space (current code default)", "https://huggingface.co/spaces/sseia/diaricore-inference"],
             ["Presentation slides", "https://docs.google.com/presentation/d/1jjBY2dVFIcDi_pvSQWGnR9x67_0Z5t7hMupsNOQbkPk/edit?usp=sharing"],
